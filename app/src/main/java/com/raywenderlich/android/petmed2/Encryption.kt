69430032e30fcfde21c0baaad75f1fe59d1b95fb
Thomas Phillips <thomasphillips3@gmail.com>
2020-07-23 22:33:56 -0700

    decrypt data

--- app/src/main/java/com/raywenderlich/android/petmed2/Encryption.kt
+++ app/src/main/java/com/raywenderlich/android/petmed2/Encryption.kt
@@ -78,7 +78,19 @@ internal class Encryption {
 
     var decrypted: ByteArray? = null
 
-    //TODO: Add code here
+    val salt = map["salt"]
+    val iv = map["iv"]
+    val encrypted = map["encrypted"]
+
+    val pbKeySpec = PBEKeySpec(password, salt, 1324, 256)
+    val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+    val keyBytes = secretKeyFactory.generateSecret(pbKeySpec).encoded
+    val keySpec = SecretKeySpec(keyBytes, "AES")
+
+    val cipher = Cipher.getInstance("AES/CBC/PKCS7Padding")
+    val ivSpec = IvParameterSpec(iv)
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
+    decrypted = cipher.doFinal(encrypted)
 
     return decrypted
   }
